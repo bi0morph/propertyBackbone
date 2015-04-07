2197d8c440ba0fbbfed7db4578f31cfe365b5408
Ruslan Ustiuhov <rusustyugov@gmail.com>
2015-04-07 20:34:46 +0300

    Add templates in AppView

--- javascripts/views/app.js
+++ javascripts/views/app.js
@@ -1,8 +1,14 @@
 var app = app || {};
 
 app.AppView = Backbone.View.extend({
+	
 	el: '#search-app',
+	
+	searchFormTemplate: _.template( $('#search-form-template').html() ),
+	propertiesListTemplate: _.template( $('#properties-list-template').html() ),
+	propertyDetailTemplate: _.template( $('#property-detail-template').html() ),
+	
 	initialize: function() {
-			
+		this.$el.append( this.searchFormTemplate() );
 	}
 });
